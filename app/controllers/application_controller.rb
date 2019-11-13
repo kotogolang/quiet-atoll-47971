ae4399e193eff10e781323d84e22e565074040f0
kotogolang <kotogolang@gmail.com>
2019-11-13 18:08:40 +0900

    Finish ch07

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_request_variant
   before_action :set_current_user
 
   def set_current_user
@@ -18,5 +19,20 @@ class ApplicationController < ActionController::Base
       redirect_to("/posts/index")
     end
   end
+
+
+private
+  def set_request_variant
+    case request.user_agent
+    when /iPad/
+      request.variant = :phone
+    when /iPhone/
+      request.variant = :phone
+    when /Android/
+      request.variant = :phone
+    end
+end
+
+
 
 end
